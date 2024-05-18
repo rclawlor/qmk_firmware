46822d4b52716750a72e887ef708020e346ced0f
rcl <ronanlawlor2001@gmail.com>
2024-05-18 13:49:39 +0100

    Add keyboard layout

--- keyboards/rcl/keymaps/default/keymap.c
+++ keyboards/rcl/keymaps/default/keymap.c
@@ -2,23 +2,30 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_uk.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+     *
+     *      0    1    2   3   4   5   6   7   8   9  10  11   12     13   14    15
+     *    ┌───┬─────┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┬─────┬─────┬─────┐
+     *  0 │Pwr│  `  │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │  -  │  =  │     │BACK │
+     *    ├───┼─────┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼─────┼─────┼─────┼─────┤
+     *  1 │M1 │ TAB │   │ / │ , │ . │ p │ y │ f │ g │ c │ r │  l  │  [  │  ]  │     │
+     *    ├───┼─────┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼─────┼─────┼─────┼─────┤
+     *  2 │M2 │CTRL │   │ a │ o │ e │ u │ i │ d │ h │ t │ n │  s  │  '  │  #  │ENTER│
+     *    ├───┼─────┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼─────┼─────┼─────┼─────┤
+     *  3 │M3 │SHIFT│ \ │ ; │ q │ j │ k │ x │ b │ m │ w │ v │  z  │     │SHIFT│     │
+     *    ├───┼─────┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼─────┼─────┼─────┼─────┤
+     *  4 │M4 │ FN  │ALT│SYS│   │S1 │   │S2 │   │S3 │   │ ? │LEFT │DOWN │ UP  │RIGHT│
+     *    └───┴─────┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴─────┴─────┴─────┴─────┘
      */
-    [0] = LAYOUT_ortho_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    [0] = LAYOUT_70(
+    //  0      1        2        3         4        5       6      7       8      9       10     11     12       13       14       15
+        KC_NO, UK_GRV,  UK_1,    UK_2,     UK_3,    UK_4,   UK_5,  UK_6,   UK_7,  UK_8,   UK_9,  UK_0,  UK_MINS, UK_EQL,  KC_NO,   KC_BSPC,
+        KC_NO, KC_TAB,  KC_NO,   UK_SLSH,  UK_COMM, UK_DOT, UK_P,  UK_Y,   UK_F,  UK_G,   UK_C,  UK_R,  UK_L,    UK_LBRC, UKRBRC,  KC_NO,
+        KC_NO, KC_LCTL, KC_NO,   UK_A,     UK_O,    UK_E,   UK_U,  UK_I,   UK_D,  UK_H,   UK_T,  UK_N,  UK_S,    UK_QUOT, UK_HASH, KC_ENT,
+        KC_NO, KC_LSFT, UK_BSLS, UK_SCLN,  UK_Q,    UK_J,   UK_K,  UK_X,   UK_B,  UK_M,   UK_W,  UK_V,  UK_Z,    KC_NO,   KC_RSFT, KC_NO,
+        KC_NO, KC_NO,   KC_LALT, KC_LWIN,  KC_NO,   KC_SPC, KC_NO, KC_SPC, KC_NO, KC_SPC, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     )
-};
+    };
